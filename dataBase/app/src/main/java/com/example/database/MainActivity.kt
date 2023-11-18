c627f4c1714827d409b944f26eaf5afa85b03437
BhaveshC07 <bhaveshcharde4321@gmail.com>
2023-11-18 10:32:31 +0530

    Database Added

---
+++ dataBase/app/src/main/java/com/example/database/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.database
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+//link database == https://console.firebase.google.com/u/0/project/database-9d06f/database/database-9d06f-default-rtdb/data
+class MainActivity : AppCompatActivity() {
+
+    lateinit var database:DatabaseReference
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val signupbtn=findViewById<Button>(R.id.btnSignUp)
+        val etName=findViewById<EditText>(R.id.etName)
+        val etMail=findViewById<EditText>(R.id.etMail)
+        val etUserName=findViewById<EditText>(R.id.etUserName)
+        val etPassword=findViewById<EditText>(R.id.etPass)
+
+        signupbtn.setOnClickListener{
+            val name=etName.text.toString()
+            val mail=etMail.text.toString()
+            val usern=etUserName.text.toString()
+            val password=etPassword.text.toString()
+
+            val user=User(name,mail,usern,password)
+            database=FirebaseDatabase.getInstance().getReference("Users")
+            database.child(usern).setValue(user).addOnSuccessListener {
+                etName.text?.clear()
+                etMail.text?.clear()
+                etUserName.text?.clear()
+                etPassword.text?.clear()
+
+                Toast.makeText(this,"User Register",Toast.LENGTH_SHORT).show()
+            }.addOnSuccessListener {
+                Toast.makeText(this,"Failed",Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
---
+++ dataBase/app/src/main/java/com/example/database/User.kt
@@ -0,0 +1,3 @@
+package com.example.database
+
+data class User(val name:String,val email:String,val userName:String,val pass:String)
